be56999f2ac4ccbe736f612df92890bb76ba08a9
aloha66 <lzcbruce@live.com>
2021-07-29 11:28:27 +0800

    feat: 新增保存和导入逻辑

--- designable/antd/playground/widgets/ActionsWidget.tsx
+++ designable/antd/playground/widgets/ActionsWidget.tsx
@@ -1,12 +1,60 @@
 import React, { useMemo } from 'react'
 import { Space, Button, Radio } from 'antd'
 import { GithubOutlined } from '@ant-design/icons'
-import { TextWidget, useWorkbench } from '@designable/react'
-
+import { TextWidget, useWorkbench, useTree } from '@designable/react'
+import { transformToSchema, transformToTreeNode } from '@designable/formily'
 import { GlobalRegistry } from '@designable/core'
 import { observer } from '@formily/react'
 
-export const ActionsWidget = observer(({ use, style, className }) => {
+const Parser = {
+  designableFormName: 'Root',
+  designableFieldName: 'DesignableField',
+}
+
+const value = {
+  form: {
+    labelCol: 6,
+    wrapperCol: 12,
+  },
+  schema: {
+    type: 'object',
+    properties: {
+      cnhzmmpzaqp: {
+        title: 'Select',
+        'x-decorator': 'FormItem',
+        'x-component': 'Select',
+        'x-validator': [],
+        'x-component-props': {
+          defaultOpen: false,
+          style: {
+            backgroundColor: 'rgba(82,63,180,1)',
+          },
+        },
+        'x-decorator-props': {
+          style: {
+            backgroundColor: 'rgba(245,166,35,1)',
+          },
+        },
+        _designableId: 'cnhzmmpzaqp',
+        'x-index': 0,
+        name: 'cnhzmmpzaqp',
+        description: 'lkl',
+      },
+    },
+    _designableId: '0p0tglq1iv3',
+  },
+}
+
+export const ActionsWidget = observer(() => {
+  const tree = useTree()
+  const handleSave = () => {
+    return JSON.stringify(transformToSchema(tree, Parser), null, 2)
+  }
+  const handlePublish = () => {
+    const payload = transformToTreeNode(value, Parser)
+    tree.from(payload)
+    tree.takeSnapshot()
+  }
   const workbench = useWorkbench()
   const previewText = useMemo(() => {
     return workbench.type === 'DESIGNABLE' ? '编辑' : '预览'
@@ -28,11 +76,11 @@ export const ActionsWidget = observer(({ use, style, className }) => {
         <GithubOutlined />
         Github
       </Button>
-      <Button>
+      <Button onClick={handleSave}>
         <TextWidget>Save</TextWidget>
       </Button>
-      <Button type="primary">
-        <TextWidget>Publish</TextWidget>
+      <Button type="primary" onClick={handlePublish}>
+        <TextWidget>import</TextWidget>
       </Button>
       <Button
         onClick={() => {
